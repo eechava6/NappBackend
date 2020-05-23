83cc19bdcdaafb00a90757aa54d14b6d916da510
eechava6 <eechava6@eafit.edu.co>
2020-05-23 16:32:52 -0500

    Adding hello

--- server.py
+++ server.py
@@ -19,6 +19,11 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
+@app.route("/",methods=['GET'])
+def hello():
+    return "<a> Hello! backend working. <a>"
+
 @app.route("/incSearch",methods=['POST'])
 def incSearch():
     data = request.json
@@ -149,4 +154,4 @@ def so():
 # Cholesky needs repairings.
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000)
+  app.run(host='0.0.0.0', port=5000, debug = False)
